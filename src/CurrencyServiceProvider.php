a5ff09530ce2e3f581c15dc83007362d6f359ba7
Giovanni Stroppa <giosfdev@gmail.com>
2020-03-12 18:24:35 -0300

    Implements Currencies package

---
+++ src/CurrencyServiceProvider.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @version 1.0
+ * @copyright (c) 2020 Kamego
+ */
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class CurrencyServiceProvider extends ServiceProvider
+{
+	/**
+	 * Register services.
+	 *
+	 * @return void
+	 */
+
+	 public function register()
+	{
+		//
+	}
+
+	/**
+	 * Bootstrap services.
+	 *
+	 * @return void
+	 */
+
+	 public function boot()
+	{
+		$this->publishes([
+			__DIR__.'/../config/subscriptions.php' => config_path('subscriptions.php')
+		]);
+	}
+}
